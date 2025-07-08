87f8bdeace71de37ad570d5414f2f07e9b749371
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-08 15:12:15 +0530

    colors

--- roboshop.sh
+++ roboshop.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="yashwanth.space"
 
 for instance in :"${INSTANCES[@]}"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0650920f370e0b14f --tag-specifications "ResourceType=instance,
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0650920f370e0b14f --tag-specifications "ResourceType=instance,
     Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
