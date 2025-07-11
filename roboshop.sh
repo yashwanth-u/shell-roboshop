0bf436ff9bc110aecf083ed410f0f5179de61f72
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-11 20:35:04 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ ZONE_ID="Z05426313QK02PI64BDTM" # replace with your ZONE ID
 DOMAIN_NAME="yashwanth.space" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
