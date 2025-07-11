61ff0563abfe7c3046aca6f3f62e3f4e3a2e72b2
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-11 12:12:14 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -28,7 +28,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
+            "Name"              : "'$instance'.'$DOMAIN_NAME'" 
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
