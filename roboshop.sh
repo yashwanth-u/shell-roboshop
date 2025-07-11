2137cbb03598e1efcd612d3231907dd74ce72321
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-11 23:23:39 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -20,21 +20,5 @@ do
     fi
     echo "$instance IP address: $IP"
 
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating or Updating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'RECORD_NAME'" 
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
-            }]
-        }
-        }]
-    }'
+    
 done
