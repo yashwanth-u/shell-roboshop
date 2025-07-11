f425e7f59336daa69dc99434359dbb5f4d8698a4
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-11 12:58:49 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01bc7ebe005fb1cb2" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z05426313QK02PI64BDTM" # replace with your ZONE ID
+DOMAIN_NAME="yashwanth.site" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
