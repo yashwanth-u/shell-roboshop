11f6c3e5df0ebba760411037ecece329e4bbb0ff
Yashwanth <yaswanthyashu100@gmail.com>
2025-07-22 19:56:17 +0530

    colors

--- mongo.sh
+++ mongo.sh
@@ -31,7 +31,7 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongodb.repo
 VALIDATE $? "Copying MongoDB repo"
 
 dnf install mongodb-org -y &>>$LOG_FILE
